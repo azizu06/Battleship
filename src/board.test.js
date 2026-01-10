ea0a23aa0eba334db2f1c121580a0c37fb5884c1
azizu06 <ab725492@ucf.edu>
2026-01-10 13:29:35 -0500

    Add test for the board class methods

---
+++ src/board.test.js
@@ -0,0 +1,38 @@
+import { Board } from './board';
+import { Ship } from './ships';
+const s1 = new Ship(4);
+const b1 = new Board();
+
+test('ship coordinate', () => {
+  b1.placeShip(s1, 3, 3, 'x');
+  expect(b1.getSquares()).toBe(4);
+});
+
+test('found ship', () => {
+  expect(b1.getSquare(3, 4)).toMatchObject({
+    length: 4,
+  });
+});
+
+test('found empty', () => {
+  expect(b1.getSquare(2, 4)).toBe(undefined);
+});
+
+test('hit ship', () => {
+  const hitSpot = b1.getAttack(3, 3);
+  const hitSpot2 = b1.getAttack(3, 3);
+  expect(hitSpot2).toBe(0);
+});
+
+test('ship dead', () => {
+  for (let i = 0; i < s1.length; i++) {
+    b1.getAttack(3, 3 + i);
+  }
+  expect(b1.allSunk()).toBe(true);
+  //   expect(s1.sunk).toBe(true);
+});
+
+test('hit water', () => {
+  b1.getAttack(2, 3);
+  expect(b1.getSquare(2, 3)).toBe(0);
+});
