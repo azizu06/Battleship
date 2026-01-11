7d1642a1d72bf9ee12bb4124693ef62a9792a5fe
azizu06 <ab725492@ucf.edu>
2026-01-10 20:17:58 -0500

    Refactor game initialization to use startGame function and remove initBoard

--- src/controller.js
+++ src/controller.js
@@ -1,29 +1,34 @@
 import { Ship } from './ships';
 import { Player } from './player';
-import { renderGrid } from './display';
 
-export function initBoard() {
-  const p1 = new Player(true);
-  const p2 = new Player(false);
-  const p1Ships = [];
-  const p2Ships = [];
+let p1 = null;
+let p2 = null;
+
+export function startGame() {
+  p1 = new Player(true);
+  p2 = new Player(false);
+  let p1Ship = null;
+  let p2Ship = null;
   for (let i = 0; i < 5; i++) {
     if (i == 2) {
-      p1Ships[i] = new Ship(3);
-      p2Ships[i] = new Ship(3);
-      p1.board.placeShip(p1Ships[i], i, i, 'x');
-      p2.board.placeShip(p2Ships[i], i, i, 'x');
+      p1Ship = new Ship(3);
+      p2Ship = new Ship(3);
+      p1.board.placeShip(p1Ship, i, i, 'x');
+      p2.board.placeShip(p2Ship, i, i, 'x');
     } else if (i > 2) {
-      p1Ships[i] = new Ship(i + 1);
-      p2Ships[i] = new Ship(i + 1);
-      p1.board.placeShip(p1Ships[i], i, i, 'x');
-      p2.board.placeShip(p2Ships[i], i, i, 'x');
+      p1Ship = new Ship(i + 1);
+      p2Ship = new Ship(i + 1);
+      p1.board.placeShip(p1Ship, i, i, 'x');
+      p2.board.placeShip(p2Ship, i, i, 'x');
     } else {
-      p1Ships[i] = new Ship(i + 2);
-      p2Ships[i] = new Ship(i + 2);
-      p1.board.placeShip(p1Ships[i], i, i, 'x');
-      p2.board.placeShip(p2Ships[i], i, i, 'x');
+      p1Ship = new Ship(i + 2);
+      p2Ship = new Ship(i + 2);
+      p1.board.placeShip(p1Ship, i, i, 'x');
+      p2.board.placeShip(p2Ship, i, i, 'x');
     }
   }
-  renderGrid(p1);
 }
+
+export const player1 = () => p1;
+
+export const player2 = () => p2;
