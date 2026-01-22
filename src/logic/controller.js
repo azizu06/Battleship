185b30a954363d3eeb50989786f2eabf6a2d9139
azizu06 <ab725492@ucf.edu>
2026-01-22 18:33:14 -0500

    Refactor game logic by consolidating controller functions and removing obsolete display code

---
+++ src/logic/controller.js
@@ -0,0 +1,73 @@
+import { Player } from '../player';
+import { aimBot, resetShots, shootRandom, resetHunt, currentHits } from './cpuHunt';
+import { placeRandom } from './shipPlacer';
+
+let p1 = new Player(true, true);
+let p2 = new Player(false, false);
+let gameRunning = false;
+
+export function randomBoard() {
+  let pt = {
+    dir: '',
+    row: 0,
+    col: 0,
+  };
+  const shipSizes = [2, 3, 3, 4, 5];
+  for (const len of shipSizes) {
+    placeRandom(len, pt);
+  }
+}
+
+export function attack(point = null) {
+  if (point) {
+    if (p2.board.getSquare(point)?.status === 1 || p2.board.getSquare(point)?.status === 0)
+      return { type: 'invalid' };
+    const res = p2.board.getAttack(point);
+    flipTurn();
+    return res;
+  } else {
+    if (currentHits().length > 0) {
+      const res = aimBot();
+      if (res.type !== 'invalid') return res;
+    }
+    return shootRandom();
+  }
+}
+
+export function flipTurn() {
+  p1.turn = !p1.turn;
+  p2.turn = !p2.turn;
+}
+
+export const player1 = () => p1;
+export const player2 = () => p2;
+export function activePlayer() {
+  return p1.turn ? p1 : p2;
+}
+
+export function gameOver(player) {
+  if (player.board.allSunk()) {
+    gameRunning = false;
+    return true;
+  }
+}
+
+export const checkGame = () => gameRunning;
+
+export function gameOn() {
+  gameRunning = true;
+}
+
+export function resetBoard() {
+  p1.board.reset();
+  p2.board.reset();
+}
+
+export function restartGame() {
+  resetHunt();
+  resetBoard();
+  resetShots();
+  gameRunning = false;
+  p1.turn = true;
+  p2.turn = false;
+}
