2fe59b80a048c7d1a11c827bbbe25ee44535f3d1
azizu06 <ab725492@ucf.edu>
2026-01-22 19:52:14 -0500

    Refactor placeRandom function to accept player parameters for ship placement

--- src/logic/shipPlacer.js
+++ src/logic/shipPlacer.js
@@ -1,19 +1,18 @@
 import { Ship } from '../ships';
-import { player1, player2 } from './controller';
 
-export function placeRandom(len, pt) {
+export function placeRandom(len, pt, p1, p2) {
   const p1Ship = new Ship(len);
   const p2Ship = new Ship(len);
   getPoint(pt);
-  while (player1().board.randomCheck(p1Ship, pt.row, pt.col, pt.dir) === false) {
+  while (p1.board.randomCheck(p1Ship, pt.row, pt.col, pt.dir) === false) {
     getPoint(pt);
   }
-  player1().board.placeShip(p1Ship, pt.row, pt.col, pt.dir);
+  p1.board.placeShip(p1Ship, pt.row, pt.col, pt.dir);
   getPoint(pt);
-  while (player2().board.randomCheck(p2Ship, pt.row, pt.col, pt.dir) === false) {
+  while (p2.board.randomCheck(p2Ship, pt.row, pt.col, pt.dir) === false) {
     getPoint(pt);
   }
-  player2().board.placeShip(p2Ship, pt.row, pt.col, pt.dir);
+  p2.board.placeShip(p2Ship, pt.row, pt.col, pt.dir);
 }
 
 function getPoint(point) {
